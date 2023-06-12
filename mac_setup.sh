e2633103dcfaa6dad8d0bdc2e45ece77c6c6d95c
Nick Pelikan <nick.pelikan@posit.co>
2023-06-12 09:50:15 -0600

    updates for homebrew and ventura

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,32 +1,39 @@
 #!/bin/sh
 
 # Laptop setup!
-# This is for macs, tested and used on Catalina
+# This is for macs, tested and used on Ventura
+# Updated: 2023-06-12
 
 # xcode developer tools
 xcode-select --install
 
 # ohmyzsh (gotta keep it all looking pretty!)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# even prettier
+sed -i 's/ZSH_THEME=.*/ZSH_THEME=\"powerlevel10k/powerlevel10k\"/g' ~.zshrc
+cp .p10k.zsh $HOME/.p10k.zsh
 
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shelling)"') >> /Users/nickp/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # iterm2
-brew cask install iterm2
+brew install --cask iterm2
 
 # python3
 brew install python3
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
+# poetry
+brew install poetry
+
 # conda
-brew cask install miniconda
+brew install --cask miniconda
 conda init zsh
 
-# pycharm
-# if this throws a hdiutil error, you likely have a process blocking hdiutil
-# rebooting can help
-brew cask install pycharm-ce
+# vscode
+brew install --cask visual-studio-code
 
 # jupyter lab (creates conda env as well)
 conda create --name jupyter -y jupyterlab nb_conda_kernels
@@ -48,6 +55,10 @@ echo "source ~/.jupyter_launcher.sh" >> ~/.zshrc
 # r
 brew install r
 
+# tidyverse and dependencies
+brew install harfbuzz fribidi freetype libpng libtiff libjpeg
+R -e "install.packages('tidyverse')"
+
 # if this throws a hdiutil error, you likely have a process blocking hdiutil
 # rebooting can help
-brew cask install rstudio
+brew install --cask rstudio
